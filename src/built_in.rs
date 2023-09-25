8799db90c6413b9744f307e0537e1d5bdac074c2
flofriday <flohacksfriday@gmail.com>
2023-09-25 21:28:29 +0200

    Add builtin functions to print numbers

--- src/built_in.rs
+++ src/built_in.rs
@@ -1,11 +1,12 @@
 use lazy_static::lazy_static;
-use std::arch::asm;
 use std::collections::HashMap;
 
 lazy_static! {
     pub static ref BUILTIN_FUNCS: HashMap<&'static str, BuiltIn> = {
         let mut map = HashMap::new();
         map.insert("cool", BuiltIn::new(0, cool_builtin as usize));
+        map.insert("show", BuiltIn::new(1, show_builtin as usize));
+        map.insert("showln", BuiltIn::new(1, showln_builtin as usize));
         map
     };
 }
@@ -21,7 +22,19 @@ impl BuiltIn {
     }
 }
 
+static DEFAULT_RETURN: i32 = 0;
+
 extern "C" fn cool_builtin() -> i32 {
     println!("Cool");
-    return 13;
+    DEFAULT_RETURN
+}
+
+extern "C" fn show_builtin(n: i32) -> i32 {
+    print!("{}", n);
+    DEFAULT_RETURN
+}
+
+extern "C" fn showln_builtin(n: i32) -> i32 {
+    println!("{}", n);
+    DEFAULT_RETURN
 }
