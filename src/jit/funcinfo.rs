9fd98804541d6e0379f50ef3af874f7b0b046091
Johannes Zottele <official@johannes-zottele.at>
2023-09-28 16:10:51 +0200

    Refactor register allocation

--- src/jit/funcinfo.rs
+++ src/jit/funcinfo.rs
@@ -2,13 +2,15 @@ use crate::ast::FuncDec;
 use crate::jit::arch_def::RegDefinition;
 use crate::jit::lir::{LirFunction, LirReg};
 use crate::jit::reg_alloc::reg_off::RegOff;
+use crate::jit::reg_alloc::RegMapping;
+use bad64::Reg;
 use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct FuncInfo<'a, D: RegDefinition> {
     name: &'a str,
     lir: LirFunction,
-    reg_alloc: HashMap<LirReg, RegOff>,
+    reg_alloc: RegMapping<D>,
 }
 
 impl<'a, D: RegDefinition> FuncInfo<'a, D> {
