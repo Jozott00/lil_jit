8ac6e6a6c49852db144ade6fef04925dd052b382
Johannes Zottele <official@johannes-zottele.at>
2023-10-03 15:27:19 +0200

    Remove unused DOCKERFILE_PATH variable from lil_jit.sh
    Removed the unnecessary variable DOCKERFILE_PATH from the shell script that runs the Docker image. Updated the script to directly use the Dockerfile path './Dockerfile' instead of referring it from a variable which was adding unnecessary complexity to the code. This change simplifies the script and makes it more robust by removing dependency on an extra variable.

--- lil_jit.sh
+++ lil_jit.sh
@@ -2,7 +2,6 @@
 
 # Define the Docker image name and Dockerfile location
 IMAGE_NAME="jozott/lil_jit:latest"
-DOCKERFILE_PATH="./Dockerfile"  # Make sure this points to your actual Dockerfile
 
 # Capture all arguments to pass to the application inside the Docker container
 APP_ARGS="$@"
@@ -28,7 +27,7 @@ fi
 # $? is a special variable that holds the exit code of the last command executed
 #if [ $? -ne 0 ]; then
 #    echo "Image does not exist, building it first..."
-#    docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH . || { echo "Docker build failed"; exit 1; }
+#    docker build -t $IMAGE_NAME -f ./Dockerfile . || { echo "Docker build failed"; exit 1; }
 #fi
 
 # Run the Docker container, conditionally mount the file, and pass the arguments to the application inside it
